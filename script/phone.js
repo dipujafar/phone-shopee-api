99478d0e9d97328a01c5bc9acf8b9c1971b649d2
dipujafar <131136004+dipujafar@users.noreply.github.com>
2023-08-29 10:50:37 +0600

    added detaile bytton

--- script/phone.js
+++ script/phone.js
@@ -1,5 +1,5 @@
 // Function to load phones based on a search query
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   // Fetch phone data from the API
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
@@ -8,23 +8,29 @@ const loadPhone = async (searchText) => {
   const phones = data.data;
 
   // Call the displayPhone function to show the loaded phones
-  displayPhone(phones);
+  displayPhone(phones, isShowAll);
 };
 
 // Function to display phones in the UI
-const displayPhone = (phones) => {
+const displayPhone = (phones, isShowAll) => {
   const phonesDiv = document.getElementById("phone-container");
   // Clear the previous content
   phonesDiv.textContent = "";
 
   // Show or hide the "Show All" button container based on the number of phones
   const showAllCOntainer = document.getElementById("show-all-container");
-  phones.length > 15
-    ? showAllCOntainer.classList.remove("hidden") // Show the container if more than 15 phones
-    : showAllCOntainer.classList.add("hidden"); // Hide the container if 15 or fewer phones
+  // Show the container if more than 15 phones
+  if (phones.length > 15 && !isShowAll) {
+    showAllCOntainer.classList.remove("hidden");
+    // Hide the container if 15 or fewer phones
+  } else {
+    showAllCOntainer.classList.add("hidden");
+  }
 
   // Slice the phone array to only display the first 15 phones
-  phones = phones.slice(0, 15);
+  if (!isShowAll) {
+    phones = phones.slice(0, 15);
+  }
   phones.forEach((phone) => {
     const phonesCard = document.createElement("div");
     phonesCard.classList = `card card-compact p-4 bg-gray-200 shadow-xl`;
@@ -39,8 +45,8 @@ const displayPhone = (phones) => {
     <div class="card-body">
       <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-center">
+        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-outline btn-info">Show Details</button>
       </div>
     </div>
     `;
@@ -51,13 +57,13 @@ const displayPhone = (phones) => {
 };
 
 // Function to handle the search button click
-const searchBtn = () => {
+const searchBtn = (isShowAll) => {
   dataLoader(true);
   const searchField = document.getElementById("search-field");
   const searchValue = searchField.value;
-  searchField.value = "";
+  // searchField.value = "";
   // Call the loadPhone function with the search value
-  loadPhone(searchValue);
+  loadPhone(searchValue, isShowAll);
 };
 
 const dataLoader = (isLoading) => {
@@ -65,4 +71,14 @@ const dataLoader = (isLoading) => {
   isLoading === true
     ? dataLoading.classList.remove("hidden")
     : dataLoading.classList.add("hidden");
+};
+
+// Show all halder
+const showAllHandler = () => {
+  searchBtn(true);
+};
+
+// Show details button
+const handleShowDetails = (id) => {
+  console.log(id);
 };
